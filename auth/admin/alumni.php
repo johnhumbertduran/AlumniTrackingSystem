5ff1b7f3382509ed47e00e1afab368283f669616
John Humbert Duran <johnhumbertd@gmail.com>
2022-01-06 22:32:37 +0800

    filters 2

--- auth/admin/alumni.php
+++ auth/admin/alumni.php
@@ -76,8 +76,10 @@ if (isset($_POST["search"])) {
 <button type="submit" name="search" class="btn btn-primary">Search</button>
 
 </div>
-<br>
-<div class="container-fluid">
+<!-- <br> -->
+
+ <!-- ######### Filters Start Here ######### -->
+<!-- <div class="container-fluid">
 <div class="form-check form-switch d-flex justify-content-start">
   <input class="form-check-input" type="checkbox" id="filterSwitch" onchange="checkFilter()" name="filter" value="yes"> &nbsp;
   <label class="form-check-label" for="filterSwitch">Allow Filter</label>
@@ -101,7 +103,7 @@ if (isset($_POST["search"])) {
 </div>
 </div>
 
-</div>
+</div> -->
 
 </form>
 
--- auth/admin/records.php
+++ auth/admin/records.php
@@ -41,7 +41,7 @@ $alumni_qry = mysqli_query($connections, "SELECT * FROM users_tbl WHERE account_
       <tr class="text-center">
         <td class="align-middle"><?php echo $idNo; ?></td>
         <td class="align-middle"><?php echo $fullName; ?></td>
-        <td class="align-middle"><?php echo $middleName; ?></td>
+        <td class="align-middle"><?php echo $homeAddress; ?></td>
         <td class="align-middle"><?php echo $sex; ?></td>
         <td class="align-middle"><?php echo $civilStatus; ?></td>
         <!-- <td class="align-middle"><?php echo $employmentAddress; ?></td>
@@ -50,10 +50,25 @@ $alumni_qry = mysqli_query($connections, "SELECT * FROM users_tbl WHERE account_
         <td class="align-middle"><?php echo $officeTelephone; ?></td>
         <td class="align-middle"><?php echo $mobileNumber; ?></td>
         <td class="align-middle"><?php echo $alumniChapterMembership; ?></td> -->
-        <td class="align-middle"><a class="btn btn-info" id="<?php echo $idNo; ?>" href="?view=<?php echo $idNo; ?>">View</a></td>
+        <td class="align-middle"><button class="btn btn-info" id="<?php echo $idNo; ?>" onclick="viewOtherInfo(this.id)">View</button></td>
         <td class="align-middle"><?php echo $paymentStatus; ?></td>
         <td class="align-middle" style="width:20%;"><a class="btn btn-success" id="<?php echo $idNo; ?>" href="?update=<?php echo $idNo; ?>">Update</a><!-- &nbsp<a class="btn btn-danger" href="?set=<?php echo $idNo; ?>">Delete</a> --></td>
       </tr>
 
+
+      <script>
+        function viewOtherInfo(userID){
+          const urlParams = new URLSearchParams(window.location.search);
+          if (history.pushState) {
+            if(!urlParams.has('view')){
+                urlParams.set('view', userID);
+                window.location.search = urlParams;
+            }
+          }
+
+        }
+      </script>
+
       <?php
     }
+    
